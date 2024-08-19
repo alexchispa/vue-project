e746f735ea8bf450e31f99ca2e8c326547c4c559
alexchispa <aleexmartii@gmail.com>
2024-08-19 10:48:16 +0200

    Primer commit, HelloWorld con Test e2e de un botón

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="saludar">Saludar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hola Mundo'
+    };
+  },
+  methods: {
+    saludar() {
+      alert('¡Hola desde Vue!');
+    }
+  }
+}
+</script>
