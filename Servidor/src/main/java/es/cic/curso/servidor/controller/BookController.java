178f5805dad6c26ea8caf449d1b17ea2fa2287b0
alexchispa <aleexmartii@gmail.com>
2024-08-26 10:41:18 +0200

    "Proyecto funcional pero con poco CSS, faltan funciones"

--- Servidor/src/main/java/es/cic/curso/servidor/controller/BookController.java
+++ Servidor/src/main/java/es/cic/curso/servidor/controller/BookController.java
@@ -1,7 +1,8 @@
 package es.cic.curso.servidor.controller;
 
+import java.util.ArrayList;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,34 +13,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.cic.curso.servidor.model.Book;
-import es.cic.curso.servidor.service.BookService;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
-    @Autowired
-    private BookService BookService;
+    private List<Book> books = new ArrayList<>();
 
     @PostMapping("/new")
     public ResponseEntity<Book> addBook(@RequestBody Book book) {
-        Book newBook = BookService.addBook(book);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newBook);
+        books.add(book);
+        return ResponseEntity.status(HttpStatus.CREATED).body(book);
     }
 
     @GetMapping
     public ResponseEntity<List<Book>> getAllBooks() {
-    List<Book> books = BookService.getAllBooks();
-    return ResponseEntity.ok(books);
+        return ResponseEntity.ok(books);
     }
 
     @GetMapping("/search")
-    public ResponseEntity<Book> getBook(
-    @RequestParam(required = true) String title,
-    @RequestParam(required = true) String author,
-    @RequestParam(required = false) int year) {
-        Book bookToFind = new Book(title, author, year);
-        Book book = BookService.getBook(bookToFind);
-        return book != null ? ResponseEntity.ok(book) : ResponseEntity.notFound().build();
+    public ResponseEntity<Book> getBook(@RequestParam String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return ResponseEntity.ok(book);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
